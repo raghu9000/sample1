598c112a44dcf5daba390a87c840d5ada911ea4f
Barelogics-Employee3 <raghuram.k@barelogics.com>
2018-11-05 12:18:00 +0530

    initial commit
    demo for test

---
+++ GeoFencing/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GeoFencing
+//
+//  Created by BLS-SivaTeja on 29/10/18.
+//  Copyright © 2018 Barelogics-Employee3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var geotifications = [Geotification]()
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
